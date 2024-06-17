426787a50dc33d3f393856f2d97e80a384cba263
Juwan King <juwanking@Juwans-MacBook-Pro.local>
2024-06-17 17:11:05 -0400

    Day 2

---
+++ test-project/src/app/features/home/home.component.ts
@@ -0,0 +1,10 @@
+// src/app/features/home/home.component.ts
+
+import { Component } from '@angular/core'
+
+@Component({
+  moduleId: module.id,
+  selector: 'ns-home',
+  templateUrl: 'home.component.html',
+})
+export class HomeComponent {}
