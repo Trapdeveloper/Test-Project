8e8e2bc8218fbb814eceba137d188dad04daf7d4
Juwan King <juwanking@Juwans-MacBook-Pro.local>
2024-06-18 16:43:30 -0400

    Day 3

--- test-project/src/app/features/home/home.component.ts
+++ test-project/src/app/features/home/home.component.ts
@@ -1,10 +1,16 @@
 // src/app/features/home/home.component.ts
 
 import { Component } from '@angular/core'
-
+import {FlickService} from '~/app/core/services/flick.service'
 @Component({
   moduleId: module.id,
   selector: 'ns-home',
   templateUrl: 'home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    flicks = this.flickService.getFlicks()
+
+    constructor(
+        private flickService: FlickService,
+    ){}
+}
